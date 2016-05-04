7f9866e036afbc51e2aaebc7e92e035283459bf0
Guoyin <guoyin.wang@duke.edu>
2016-05-03 22:47:57 -0400

    add get page link

--- week2/week2_homework/ganji_guoyin/channel_extract.py
+++ week2/week2_homework/ganji_guoyin/channel_extract.py
@@ -5,6 +5,10 @@ import pymongo
 start_url = 'http://bj.ganji.com/wu/'
 url_add = 'http://bj.ganji.com'
 
+client = pymongo.MongoClient('localhost', 27017)
+ganji = client['ganji']
+page_link = ganji['page_link']
+
 def get_channel_url(start_url, ):
 
     wb_data = requests.get(start_url)
@@ -18,8 +22,30 @@ def get_channel_url(start_url, ):
 
     return channel_url
 
+def get_link_url(page_url):
+    page_url_set = [page_url + 'o{}'.format(i) for i in range(100)]
+
+    for page in page_url_set:
+        wb_data = requests.get(page)
+        soup = BeautifulSoup(wb_data.text, 'lxml')
+        links = soup.select('li.js-item > a.ft-tit')
+        for link in links:
+            url = link.get('href')
+            name = link.text.strip()
+            data = {'name' : name,
+                    'url' : url}
+            print(data)
+            if page_link.find(data).count() == 0:
+                print('insert')
+                page_link.insert_one(data)
+            else:
+                print('exist')
+                continue
+
+
 
 
 
 if __name__ == '__main__':
-    channel_url = get_channel_url(start_url)
+    # channel_url = get_channel_url(start_url)
+    get_link_url('http://bj.ganji.com/jiaju/')
